83aedc4b7c16f5efa58130dec7c365b704fe89d9
Adailson Santos <adailson2@gmail.com>
2018-06-11 18:21:12 -0300

    Server initiated

---
+++ db.py
@@ -0,0 +1,22 @@
+import datetime 
+from sqlalchemy import create_engine, Column, Integer, Boolean, Float, Unicode, Sequence, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+engine = create_engine("postgresql://greenhouse:greenhouse@postgres:5432/greenhouse", echo=True)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+Base = declarative_base()
+
+class DrawerStatus(Base):
+
+    __tablename__ = 'drawerStatus'
+
+    id = Column(Integer, 
+            Sequence('drawer_status_id_seq'), primary_key=True)
+    value = Column(Boolean)
+    created_date = Column(DateTime, default=datetime.datetime.utcnow) 
+
+Base.metadata.create_all(engine)
---
+++ drawer_status_server.py
@@ -0,0 +1,16 @@
+from nameko.rpc import rpc
+import db
+from db import DrawerStatus
+
+
+class DrawerStatusServer():
+    name = "drawer_status_server"
+
+    @rpc
+    def receive_temperature(self, drawer_status):
+        drawer_status = round(drawer_status, 1)
+        d = DrawerStatus()
+        d.value = drawer_status
+        db.session.add(d)
+        db.session.commit()
+        return drawer_status
